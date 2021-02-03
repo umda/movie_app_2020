40055d5113530893e0c2564b80e3176b6b21eb93
umda <abcdeomdain@gamil.com>
2021-02-03 17:31:01 +0900

    Updates

---
+++ precache-manifest.2a49ccaa9b18d160584e6a689017692c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d97d4631495d4f2ed069297cdd99de",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "00e70f8cd65e7736ac45",
+    "url": "/movie_app_2020/static/css/main.0fba1c0d.chunk.css"
+  },
+  {
+    "revision": "b7750ce7d3c365722574",
+    "url": "/movie_app_2020/static/js/2.44e27315.chunk.js"
+  },
+  {
+    "revision": "00e70f8cd65e7736ac45",
+    "url": "/movie_app_2020/static/js/main.b3055988.chunk.js"
+  },
+  {
+    "revision": "68c916e7b3c11eb2f156",
+    "url": "/movie_app_2020/static/js/runtime~main.d1c1cce6.js"
+  }
+]);
